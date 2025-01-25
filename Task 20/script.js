b34e870dda7efeee276336af9e47ce77e2c6a411
Ahmad-U19 <ahmedoppal2592@gmail.com>
2025-01-24 23:37:00 -0800

    Task 20

---
+++ Task 20/script.js
@@ -0,0 +1,14 @@
+function removeDuplicates(arr) {
+    let uniqueArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    
+    return uniqueArr;
+}
+
+// Example usage
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));  // [1, 2, 3, 4, 5]
